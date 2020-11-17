55467bcb33ca3a379ce4b402f287987734179df6
Dom G <21165092+dgrisafe@users.noreply.github.com>
2020-11-17 09:54:15 -0800

    Added Program

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # zotero2ncbi
-Format Zotero References Exported as CSV into NCBI Batch Citation Matcher
+
+Format **Zotero** References Exported as CSV into **NCBI Batch Citation Matcher**
+
+This process was required by the American Journal of Ophthalmology to verify references with PubMed before submitting the manuscript. 
+It was impossible to find a simple way to do this, so this is a crude attempt to automate an otherwise mundane task.
+
+
+# Zotero
+
+Create list of references in [Zotero Reference Manager](https://www.zotero.org/).
+
+Highlight references, right click, **Export items...** with **Format: CSV**. 
+
+
+# R Script
+
+Run R script, selecting .CSV file exported from Zotero
+
+
+# NCBI Batch Citation Manager
+
+Import processed .CSV fie into [NCBI Batch Citation Manager](https://www.ncbi.nlm.nih.gov/pubmed/batchcitmatch)
---
+++ zotero2ncbi.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(rstudioapi)
+
+
+# function to select .csv file location
+get_csv_path <- function(x){
+  
+  # prompt dialogue box to select .csv file of exported references 
+  request_path <- file.choose()
+  
+  if(grepl(request_path, pattern = "*.csv$")){
+    request_path
+  }else{
+    warning('Must be .CSV file. Please try again.')
+    get_csv_path()
+  }
+}
+path_name <- get_csv_path()
+
+# read .csv file
+zotero_refs <- read.csv(path_name)
+zotero_refs %>% glimpse
+
+# format for NCBI Batch Citation Manager:
+#    journal_title|year|volume|first_page|author_name|your_key|
+ncbi_batch_format <- zotero_refs %>% 
+  dplyr::mutate(
+    ncbi_string = paste0(
+      paste(sep = "|",
+            Journal.Abbreviation, Publication.Year, Volume, 
+            str_extract(Pages, "^\\d+"), Author, Key), 
+      "|"
+      )
+    )
+
+
+ncbi_batch_format$ncbi_string %>% head
+
+# save as .txt file to upload to NCBI at:
+#   https://www.ncbi.nlm.nih.gov/pubmed/batchcitmatch
+write.table(
+  ncbi_batch_format$ncbi_string, file = paste0("refs_ncbi_batch_citation_manager_", Sys.Date(), ".txt"),
+  sep = "\t", row.names = FALSE, col.names = FALSE
+  )
